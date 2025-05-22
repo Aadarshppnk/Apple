7e5e0a4b1dcf38f02080ff2d60a813f519a8d40e
Aadarshppnk <neonchan016@gmail.com>
2025-05-22 19:56:23 +0530

    Update Cluster.js

--- Cluster.js
+++ Cluster.js
@@ -1,3 +1,20 @@
 // Copyright (c) 2025 Aadarsh Raj Kunwer
 // Project: Apple Bot
 // All rights reserved. Do not use, copy, modify, or distribute without permission.
+
+const { ClusterManager } = require('discord-hybrid-sharding');
+const path = require('path');
+const config = require('./config.json');
+
+const manager = new ClusterManager(path.resolve(__dirname, './bot.js'), {
+  totalShards: 'auto',
+  totalClusters: 'auto',
+  mode: 'process',
+  token: config.token,
+});
+
+manager.on('clusterCreate', cluster => {
+  console.log(`[Cluster Manager] Launched Cluster #${cluster.id}`);
+});
+
+manager.spawn({ timeout: -1 });
